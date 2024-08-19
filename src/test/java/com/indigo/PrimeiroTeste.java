608a0ed510462c28587649ef20eeeeb37e32f410
Indigo Ziroldo <2411600131@fema.edu.br>
2024-08-19 16:29:02 -0300

    feat: implementar soma e não somar numeros negativos

--- src/test/java/com/indigo/PrimeiroTeste.java
+++ src/test/java/com/indigo/PrimeiroTeste.java
@@ -1,5 +1,6 @@
 package com.indigo;
 
+import com.sun.imageio.plugins.common.I18N;
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,14 +8,35 @@ import org.junit.Test;
 public class PrimeiroTeste {
 
     @Test
-    public void estruturaDeUmTeste(){
+    public void deveSomar2Numeros(){
         //cenário
+        Calculadora calculadora = new Calculadora();
         int numero1 = 10, numero2 = 5;
 
         //execução
-        int resultado = numero1 + numero2;
+        int resultado = calculadora.somar(numero1, numero2);
 
         //verificação
-        Assertions.assertThat(resultado).isBetween(14,16);
+        Assertions.assertThat(resultado).isEqualTo(15);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void naoDeveSomarNumerosNegativos(){
+        //cenario
+        Calculadora calculadora = new Calculadora();
+        int num1 = -10, num2 = 5;
+
+        //execução
+        calculadora.somar(num1, num2);
+    }
+}
+
+class Calculadora{
+
+    int somar(int num1, int num2){
+        if(num1 < 0 || num2 < 0){
+            throw new RuntimeException("Não é permitido somar numeros negativos.");
+        }
+        return num1 + num2;
     }
 }
